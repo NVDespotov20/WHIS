2b8ceae4cfd1652ae429163d3a981f67e53684d8
Nikolay Despotov <85335497+NVDespotov20@users.noreply.github.com>
2023-11-22 23:49:26 +0200

    Clean up esp code

--- ESP-Client/ESP-Client.ino
+++ ESP-Client/ESP-Client.ino
@@ -4,18 +4,22 @@
 
 const char *ssid = "Intercom";
 const char *password = "iNTERcom";
-const char *serverIP = "10.42.0.1";
-const unsigned int serverPort = 6948;
+IPAddress serverIP(10,42,0,1);
+const int serverPort = 6948;
 
 const int sampleRate = 8000;
 const int bitsPerSample = 16;
 const int channels = 1;
 const int bufferSize = 1024;
-
-char* packetBuffer;
+char *buf;
+char *bufReceive;
 //AudioData audioData;
 
 WiFiUDP udp;
+
+void sampleMicrophone(char buf[]);
+void playAudio(char buf[]);
+void sendPacket(char buf[]);
 
 void setup() {
   Serial.begin(115200);
@@ -27,44 +31,57 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
 
-  Serial.println("Connected to WiFi");
+  Serial.printf("Connected to %s with IP: %s", ssid, WiFi.localIP().toString());
+  serverIP = WiFi.gatewayIP();
+
+  //init buffers
+  buf = new char[bufferSize];
+  bufReceive = new char[bufferSize];
   
   //Set up udp
-  packetBuffer = new char[bufferSize];
   udp.begin(serverPort);
-  // Set up the audio library
-  //Audio.begin(sampleRate, bitsPerSample, channels);
+
+  Serial.println("Init done");
+}
+int counter = 0;
+void loop() 
+{
+  while(true)
+  {
+    sampleMicrophone(buf, counter);
+    sendPacket(buf);
+    receivePacket(bufReceive);
+    playAudio(bufReceive);
+  }
 }
 
-void loop() {
-  /*// Capture audio from the microphone
-  int16_t buffer[bufferSize];
-  int bytesRead = Audio.read(buffer, bufferSize);
+void sampleMicrophone(char buf[], int &lastEl)
+{
+  float sample = analogRead(A0);
+  sample = map(sample, 0,1023,0,255);
+  if(lastEl >= bufferSize)
+    return;
+  buf[++lastEl] = sample;
+  
+}
 
-  if (bytesRead > 0) {
-    // Send audio data to the Raspberry Pi
-    udp.beginPacket(serverIP, serverPort);
-    udp.write((uint8_t *)buffer, bytesRead * 2); // 2 bytes per sample
-    udp.endPacket();
-    Serial.println("Audio data sent to Raspberry Pi");
-  */
-
-  // Check for incoming audio data from the Raspberry Pi
-  int packetSize = udp.parsePacket();
-  if (packetSize > 0) {
-    int bytesRead = udp.read(packetBuffer, bufferSize);
-    if (bytesRead > 0) {
-      // Play the received audio data on the speaker
-      //Audio.write(buffer, bytesRead / 2); // 2 bytes per sample
-      Serial.print("Message from pi: ");
-      for(int i = 0; i < packetSize; ++i)
-        Serial.print(packetBuffer[i]);
-      Serial.println("Audio data received from Raspberry Pi");
-    }
-  }
-
-  //Send Packet
-  udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-  udp.write(replyPacket);
+void sendPacket(char buf[])
+{
+  udp.beginPacket(serverIP, serverPort);
+  udp.write(buf);
   udp.endPacket();
 }
+
+void receivePacket(char buf[])
+{
+  int packetSize = udp.parsePacket();
+  if (packetSize <= 0)
+    return;
+  int bytesRead = udp.read(buf, bufferSize);
+  buf[bytesRead] = '\0';
+  
+}
+void playAudio(char buf[])
+{
+  
+}           
