ff18db5fac917e8ab874c451a349234eabe815b2
Nikolay Despotov <85335497+NVDespotov20@users.noreply.github.com>
2023-11-30 23:11:32 +0200

    Add back-end foundation

---
+++ website/website/script.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io(); // Assuming you've included the Socket.io script in your HTML
+
+  const appendMessage = (message, sender) => {
+    const chatMessages = document.getElementById("chat-messages");
+    const messageElement = document.createElement("div");
+    messageElement.className =
+      sender === "You" ? "sent-message" : "received-message";
+    messageElement.classList.add("chat-message");
+    messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    chatMessages.appendChild(messageElement);
+  };
+
+  const appendSystemMessage = (message) => {
+    const chatMessages = document.getElementById("chat-messages");
+    const messageElement = document.createElement("div");
+    messageElement.className = "system-message";
+    messageElement.innerText = message;
+    chatMessages.appendChild(messageElement);
+  };
+
+  document
+    .getElementById("new-stranger-button")
+    .addEventListener("click", () => {
+      const chatMessages = document.getElementById("chat-messages");
+      chatMessages.innerHTML = "";
+      scrollChatToBottom();
+      appendSystemMessage(
+        "You requested a new stranger. Waiting for a match..."
+      );
+    });
+
+  document.getElementById("send-button").addEventListener("click", () => {
+    sendMessage();
+  });
+
+  document
+    .getElementById("message-input")
+    .addEventListener("keydown", (event) => {
+      if (event.key === "Enter" && !event.shiftKey) {
+        // Prevent the default behavior of the Enter key (e.g., adding a new line)
+        event.preventDefault();
+        sendMessage();
+        scrollChatToBottom();
+      }
+    });
+
+  document
+    .getElementById("chat-messages")
+    .addEventListener("touchstart", function () {
+      userTouchedScroll = true;
+    });
+
+  function sendMessage() {
+    const messageInput = document.getElementById("message-input");
+    const message = messageInput.value.trim();
+
+    if (message !== "") {
+      // Send the message to the backend
+      socket.emit("chat message", message);
+      // Append the sent message to the chat window
+      appendMessage(message, "You");
+
+      messageInput.value = "";
+
+      if (!userTouchedScroll) {
+        scrollChatToBottom();
+      }
+    }
+  }
+
+  function scrollChatToBottom() {
+    chatMessages.scrollTo({
+      top: chatMessages.scrollHeight,
+      behavior: "smooth",
+    });
+  }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      newStrangerButton.click();
+    }
+  });
+
+  socket.on("chat message", ({ message, sender }) => {
+    // Append the received message to the chat window
+    appendMessage(message, sender);
+  });
+});
