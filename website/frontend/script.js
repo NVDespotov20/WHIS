3325c129a81eabb756dcf0175820546f42615aa6
NVDespotov20 <nvdespotov20@codingburgas.bg>
2023-12-02 01:27:27 +0200

    Add two way comunication and random person arlorithm

--- website/frontend/script.js
+++ website/frontend/script.js
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const socket = io(); // Assuming you've included the Socket.io script in your HTML
 
+  let clientId;
+
   const appendMessage = (message, sender) => {
     const chatMessages = document.getElementById("chat-messages");
     const messageElement = document.createElement("div");
+
     messageElement.className =
-      sender === "You" ? "sent-message" : "received-message";
+      sender === clientId ? "sent-message" : "received-message";
     messageElement.classList.add("chat-message");
-    messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    sender = sender === clientId ? "You" : "Stranger";
+    if (typeof message === 'object' && message.message) {
+      // If the message is an object, assume it has 'message' property
+      messageElement.innerHTML = `<strong>${sender}:</strong> ${message.message}`;
+    } else {
+      messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    }
   
     // Append the new message at the end of the chat
     chatMessages.appendChild(messageElement);
@@ -24,6 +33,10 @@ document.addEventListener("DOMContentLoaded", () => {
     chatMessages.appendChild(messageElement);
   };
 
+  socket.emit('clientIdentifier', clientId);
+
+  console.log(`User connected with ID: ${clientId}`);
+
   document
     .getElementById("new-stranger-button")
     .addEventListener("click", () => {
@@ -33,6 +46,13 @@ document.addEventListener("DOMContentLoaded", () => {
       appendSystemMessage(
         "You requested a new stranger. Waiting for a match..."
       );
+      socket.emit('newStrangerRequest');
+    });
+  
+    socket.on('connectionEstablished', ({ user1, user2 }) => {
+      console.log(`Connection established between ${user1} and ${user2}`);
+      // Handle the new connection, e.g., display a message, update UI, etc.
+      appendSystemMessage("You are now connected with a new stranger!");
     });
 
   document.getElementById("send-button").addEventListener("click", () => {
@@ -61,10 +81,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const message = messageInput.value.trim();
 
     if (message !== "") {
-      // Send the message to the backend
-      socket.emit("chat message", message);
+      // Send the message to the backend along with the sender's ID
+      socket.emit("chat message", { message, sender: clientId });
       // Append the sent message to the chat window
-      appendMessage(message, "You");
+      appendMessage(message, clientId);
 
       messageInput.value = "";
 
@@ -87,6 +107,10 @@ document.addEventListener("DOMContentLoaded", () => {
     if (event.key === "Escape") {
       newStrangerButton.click();
     }
+  });
+
+  socket.on("clientIdentifier", (id) => {
+    clientId = id;
   });
 
   socket.on("chat message", ({ message, sender }) => {
--- website/index.js
+++ website/index.js
@@ -1,48 +1,244 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cookieParser = require('cookie-parser');
+const { v4: uuidv4, validate: isUuidValid } = require('uuid');
+const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'website' folder
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static('frontend'));
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+const dbConfig = {
+  server: 'localhost',
+  user: 'whis',
+  password: 'SecurePassMSWord2029',
+  database: 'WHIS',
+  options: { 
+    encrypt: true, // Use this if you're on Windows Azure
+    trustServerCertificate: true,
+  },
+};
+
+app.get('*', (req, res) => {  
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
-// Store connected clients
-const connectedClients = {};
+async function checkClientIdExistsInDatabase(clientId) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request()
+      .input('clientId', sql.VarChar, clientId)
+      .query('SELECT COUNT(*) AS count FROM Users WHERE UUID = @clientId');
 
-io.on("connection", (socket) => {
-  console.log("User connected");
+    return result.recordset[0].count > 0;
+  } catch (error) {
+    console.error('Error checking clientId in the database:', error.message);
+    throw error;
+  }
+}
 
-  // Store the socket ID of the connected client
-  const socketId = socket.id;
-  connectedClients[socketId] = socket;
+async function getSocketId(clientId) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request()
+      .input('clientId', sql.VarChar, clientId)
+      .query('SELECT SocketId FROM Users WHERE UUID = @clientId');
+
+    return result.recordset[0].SocketId;
+  } catch (error) {
+    console.error('Error getting socket id from the database:', error.message);
+    throw error;
+  }
+}
+
+
+async function insertClientIdIntoDatabase(clientId, socketId) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    await pool.request()
+      .input('uuid', sql.VarChar, clientId)
+      .input('socketId', sql.VarChar, socketId)
+      .query('INSERT INTO Users (UUID, IsActive, IsSearching, socketId) VALUES (@uuid, \'true\', \'true\', @socketId)');
+  } catch (error) {
+    console.error('Error inserting clientId into the database:', error.message);
+    throw error;
+  }
+}
+async function makeClientIdInactiveInDatabase(clientId) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    await pool.request()
+      .input('uuid', sql.NVarChar, clientId)
+      .query('UPDATE Users SET IsActive = \'false\' WHERE UUID = @uuid');
+  } catch (error) {
+    console.error('Error inserting clientId into the database:', error.message);
+    throw error;
+  }
+}
+
+async function updateClientSearchingStatus(clientId, isSearching) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    await pool.request()
+      .input('uuid', sql.VarChar, clientId)
+      .input('isSearching', sql.VarChar, isSearching)
+      .query('UPDATE Users SET IsSearching = @isSearching WHERE UUID = @uuid');
+  } catch (error) {
+    console.error('Error updating IsSearching in the database:', error.message);
+    throw error;
+  }
+}
+
+async function getSearchingUsers() {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request()
+      .query('SELECT UUID FROM Users WHERE IsActive = \'true\' AND IsSearching = \'true\'');
+
+      const uuids = result.recordset.map((row) => row.UUID);
+
+      // Use Promise.all to await all asynchronous operations
+      const users = await Promise.all(uuids.map(async (uuid) => {
+        return uuid;
+      }));
+  
+      return users;
+  } catch (error) {
+    console.error('Error getting active users from the database:', error.message);
+    throw error;
+  }
+}
+
+async function getOtherUserInConnection(clientId) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request()
+      .input('clientId', sql.VarChar, clientId)
+      .query('SELECT ClientOneUUID, ClientTwoUUID FROM Connections WHERE ( ClientOneUUID = @clientId OR ClientTwoUUID = @clientId ) AND IsActive = \'true\'');
+    if(result.recordset.length == 0)
+      return null;
+    if(result.recordset[0].ClientOneUUID == clientId)
+      return result.recordset[0].ClientTwoUUID;
+    else return result.recordset[0].ClientOneUUID;
+  } catch (error) {
+    console.error('Error getting other user in connection from the database:', error.message);
+    throw error;
+  }
+}
+async function createConnectionInDatabase(clientOneUUID, clientTwoUUID) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    await pool.request()
+      .input('clientOneUUID', sql.VarChar, clientOneUUID)
+      .input('clientTwoUUID', sql.VarChar, clientTwoUUID)
+      .query('INSERT INTO Connections (ClientOneUUID, ClientTwoUUID, IsActive) VALUES (@clientOneUUID, @clientTwoUUID, \'true\')');
+  } catch (error) {
+    console.error('Error creating connection in the database:', error.message);
+    throw error;
+  }
+}
+async function getIdOfConnection(clientId) {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request()
+      .input('clientId', sql.VarChar, clientId)
+      .query('SELECT Id FROM Connections WHERE ( ClientOneUUID = @clientId OR ClientTwoUUID = @clientId ) AND IsActive = \'true\'');
+      if(result.recordset[0])
+        return result.recordset[0].Id;
+      else return -1;
+  } catch (error) {
+    console.error('Error creating connection in the database:', error.message);
+    throw error;
+  }
+}
+
+async function makeConnectionInactive(conId) {
+  const pool = await sql.connect(dbConfig);
+  const result = await pool.request()
+    .input('conId', sql.Int, conId)
+    .query('UPDATE Connections SET IsActive = \'false\' WHERE Id = @conId');
+}
+
+async function connectStrangers() {
+  const clients = await getSearchingUsers();
+  if(clients.length > 1) {
+    const clientOne = clients[Math.floor(Math.random()*clients.length)];
+    const clientTwo = clients[Math.floor(Math.random()*clients.length)];
+    if(clientOne != clientTwo) {
+      createConnectionInDatabase(clientOne, clientTwo);
+      updateClientSearchingStatus(clientOne, 'false');
+      updateClientSearchingStatus(clientTwo, 'false');
+      io.to(clientOne).to(clientTwo).emit('connectionEstablished', { user1: clientOne, user2: clientTwo });
+      console.log(`Connection established between ${clientOne} and ${clientTwo}`);
+    }
+  }
+}
+
+io.on('connection', async (socket) => {
+  let clientId = socket.handshake.headers.cookie
+    ? socket.handshake.headers.cookie.split('; ').find(row => row.startsWith('clientId='))
+    : null;
+
+  if (!clientId || !isUuidValid(clientId.split('=')[1]) || !(await checkClientIdExistsInDatabase(clientId.split('=')[1]))) {
+    clientId = `clientId=${uuidv4()}`;
+    socket.handshake.headers.cookie = clientId;
+
+    await insertClientIdIntoDatabase(clientId.split('=')[1], socket.id);
+  }
+
+  clientId = clientId.split('=')[1];
+  socket.emit('clientIdentifier', clientId);
+
+  console.log(`User connected with ID: ${clientId} on socket: ${socket.id}`);
 
   // Listen for chat messages
-  socket.on("chat message", (msg) => {
-    console.log(`Message: ${msg}`);
+  socket.on('chat message', async ({ message }) => {
+    const connectionExists = await getIdOfConnection(clientId);
+    if (connectionExists != -1) {
+      console.log('connectionExists with id: ' + connectionExists);
+      // If the user is part of an active connection, get the other user in the connection
+      const otherUser = await getOtherUserInConnection(clientId);
+      
+      if (otherUser) {
+        const otherUserSocketId = await getSocketId(otherUser);
+        console.log('otherUser: ' + otherUser);
+        console.log('message: ' + message);
+        // Emit the message only to the two users in the connection
+        io.to(otherUserSocketId).emit('chat message', { message, sender: clientId });
+      }
+    }
+  });
 
-    // Broadcast the message to all connected clients except the sender
-    socket.broadcast.emit("chat message", { message: msg, sender: socketId });
+  // Handle the request for a new stranger
+  socket.on('requestNewStranger', async (requestingClientId) => {
+    // Check if the requesting client already has an active connection
+    const connectionId = await getIdOfConnection(requestingClientId);
+
+    if (connectionId != -1) {
+      makeConnectionInactive(connectionId);
+    }
+    // Update the requesting client's IsSearching to 'true'
+    await updateClientSearchingStatus(requestingClientId, 'true');
   });
 
   // Handle disconnection
-  socket.on("disconnect", () => {
-    console.log("User disconnected");
-
-    // Remove disconnected client from the connectedClients object
-    delete connectedClients[socketId];
+  socket.on('disconnect', async () => {
+    console.log(`User with ID ${clientId} disconnected`);
+    await makeClientIdInactiveInDatabase(clientId);
+    const connectionId = await getIdOfConnection(clientId);
+    if(connectionId)
+      await makeConnectionInactive(connectionId);
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+var timer = setInterval(connectStrangers, 5000);
