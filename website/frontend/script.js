78490666509e4e1ed2913d4a735d346b68011fed
Александър Костов <AIKostov20@codingburgas.bg>
2023-12-02 09:41:04 +0200

    minor changes

--- website/frontend/script.js
+++ website/frontend/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const socket = io(); // Assuming you've included the Socket.io script in your HTML
-
+  const socket = io(); 
   let clientId;
 
   const appendMessage = (message, sender) => {
@@ -12,16 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
     messageElement.classList.add("chat-message");
     sender = sender === clientId ? "You" : "Stranger";
     if (typeof message === 'object' && message.message) {
-      // If the message is an object, assume it has 'message' property
-      messageElement.innerHTML = `<strong>${sender}:</strong> ${message.message}`;
+          messageElement.innerHTML = `<strong>${sender}:</strong> ${message.message}`;
     } else {
       messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
     }
   
-    // Append the new message at the end of the chat
+    
     chatMessages.appendChild(messageElement);
-  
-    // Scroll to the bottom after appending a new message
     scrollChatToBottom();
   };
   
@@ -51,7 +47,6 @@ document.addEventListener("DOMContentLoaded", () => {
   
     socket.on('connectionEstablished', ({ user1, user2 }) => {
       console.log(`Connection established between ${user1} and ${user2}`);
-      // Handle the new connection, e.g., display a message, update UI, etc.
       appendSystemMessage("You are now connected with a new stranger!");
     });
 
@@ -63,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .getElementById("message-input")
     .addEventListener("keydown", (event) => {
       if (event.key === "Enter" && !event.shiftKey) {
-        // Prevent the default behavior of the Enter key (e.g., adding a new line)
+      
         event.preventDefault();
         sendMessage();
         scrollChatToBottom();
@@ -81,15 +76,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const message = messageInput.value.trim();
 
     if (message !== "") {
-      // Send the message to the backend along with the sender's ID
+      
       socket.emit("chat message", { message, sender: clientId });
-      // Append the sent message to the chat window
+      
       appendMessage(message, clientId);
 
       messageInput.value = "";
 
       if (!userTouchedScroll) {
-        // Scroll to the bottom after sending a new message
         scrollChatToBottom();
       }
     }
@@ -114,7 +108,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   socket.on("chat message", ({ message, sender }) => {
-    // Append the received message to the chat window
     appendMessage(message, sender);
   });
 });
